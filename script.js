3a625e9cff0cac8613df70d7fdeb38727dcaf746
DavidLingvall <36515541+DavidLingvall@users.noreply.github.com>
2018-09-24 11:54:03 +0200

    David

--- script.js
+++ script.js
@@ -6,17 +6,24 @@ var i = 0;
 
 function bytBild(){
   if (i < images.length) {
-    document.getElementById("bildSpel").setAttribute("src", images[i]);
+    $('#bildSpel').attr('src', images[i]);
     i++;
   }
   else if (i == images.length) {
     i=0;
-    document.getElementById("bildSpel").setAttribute("src", images[i]);
+    $('#bildSpel').attr('src', images[i]);
     i++;
   }
 }
 function timer(){
-  setInterval(bytBild, 2000);
-}
+  //setInterval(bytBild, 2000);
 
-$
+}
+$("#bildSpel").change(function(){
+    var selected = $(this).val();
+    var image = $("#bildSpel");
+    image.fadeOut('fast', function () {
+        image.attr('src', '/assets/images/mini/'+selected+'.png');
+        image.fadeIn('fast');
+    });
+});
